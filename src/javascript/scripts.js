5f54c39575f21eee85e0db3ac45a1bf3cc8c3aeb
Willian Gabriel <willianlopes20@hotmail.com>
2025-12-28 10:24:49 -0300

    finish scheduling section

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-   $('#mobile-btn').on('click', function(){
-        $('#mobile-menu').toggleClass('active');
-        $('#mobile-btn').find('i').toggleClass('fa-bars fa-xmark');
-   });
+$(document).ready(function () {
+  $("#mobile-btn").on("click", function () {
+    $("#mobile-menu").toggleClass("active");
+    $("#mobile-btn").find("i").toggleClass("fa-bars fa-xmark");
+  });
+
+  const sections = $("section");
+  const navItems = $(".nav-item");
+
+  $(window).on("scroll", function () {
+    const scrollPosition = $(this).scrollTop();
+
+    let activeIndex = 0;
+
+    sections.each(function (i) {
+      const top = $(this).offset().top - 120;
+      const bottom = top + $(this).outerHeight();
+
+      if (scrollPosition >= top && scrollPosition < bottom) {
+        activeIndex = i;
+        return false;
+      }
+    });
+
+    navItems.removeClass("active");
+    navItems.eq(activeIndex).addClass("active");
+  });
 });
